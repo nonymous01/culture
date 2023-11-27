da3232fe12c5e9b8437ee06aa56071f16f47479b
nonymous <youssef.anonymous0@gmail.com>
2023-11-27 16:41:34 +0000

    ee

--- auth_app/form.py
+++ auth_app/form.py
@@ -4,18 +4,18 @@ from django.contrib.auth.forms import UserCreationForm
 
 class AuthUser(UserCreationForm):
     email = forms.EmailField(
-        label="email",
+        label="entrer un e-mail",
         required=True,
         widget=forms.TextInput(attrs={'autocomplete': 'email'}),
-        #help_text="Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.",
+        #help_text="Required. 150 characters or fewer. Lettersand @/./+/-/_ only.",
     )
     password1 = forms.CharField(
-        label="password",
+        label="entrer un mot de passe",
         widget=forms.PasswordInput(attrs={'autocomplete': 'new-password'}), 
     )
 
     password2 = forms.CharField(
-        label="password confirmation",
+        label="confirmation de mot de passe",
         widget=forms.PasswordInput(attrs={'autocomplete': 'new-password'}), 
     )
 #class meta pour personnaliser les chants du formulaire
